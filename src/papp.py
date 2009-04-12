c964c4d804c2c1fcb4d5ebdf414c05b974612f4a
javsmo <javsmo@6f7cd686-250c-11de-9078-ef7f9e05793c>
2009-04-12 22:02:33 +0000

    Change the window_list to a dictionary Starts the implementation of windows focus

--- src/papp.py
+++ src/papp.py
@@ -15,6 +15,9 @@ class PApp(object):
 
     def __init__(self):
         app.screen = "full"
+        self.window_list = {}
+        self.active_handle = 0 #New windows will get sequential handles from this variable
+        self.active_focus = 0
         self.canvas_ctrl = Canvas(redraw_callback = self.redraw,event_callback = self.event,resize_callback = self.resize)
         self.size = sysinfo.display_pixels()
         self.canvas = graphics.Image.new(self.size)
@@ -44,18 +47,26 @@ class PApp(object):
             w=updt['win']
             self.canvas.blit(w.get_canvas(),target=w.get_position(),source=((0,0),w.get_size()))
         self.canvas_ctrl.blit(self.canvas)
-        
+    
+    def new_handle(self):
+        self.active_handle += 1
+        return self.active_handle
+    
     def add_window(self,win):
-        self.window_list.append(win)
+        handle = self.new_handle()
+        self.window_list[str(handle)] = win
+        self.active_focus = handle
+        return handle
 
     def redraw_window(self,w):
         self.canvas.blit(w.get_canvas(),target=w.get_position(),source=((0,0),w.get_size()))
         self.canvas_ctrl.blit(self.canvas)
-        
+
     def redraw(self,rect=None):
         if self.background:
             self.canvas.blit(self.background)
-        for w in self.window_list:
+        for k in self.window_list:
+            w = self.window_list[k]
             self.canvas.blit(w.get_canvas(),target=w.get_position(),source=((0,0),w.get_size()))
         self.canvas_ctrl.blit(self.canvas)
 
@@ -65,8 +76,33 @@ class PApp(object):
     def resize(self,rect):
         pass
 
-    def set_focus(self):
-        pass
+    def set_focus(self, handle):
+        self.release_focus(self.active_focus, 0)
+        self.active_focus = handle
+        w = self.window_list[handle]
+        w.draw_focus()
+
+    def release_focus(self, handle, direction):
+        if handle != 0:
+            if direction == 0:
+                #don't focus any window
+                self.active_focus = 0
+                w = self.window_list[handle]
+                w.clear_focus()
+            elif direction < 0:
+                # Focus previous window
+                k = self.window_list.keys()
+                i = (k.index(handle) + 1) % len(k)
+                self.set_focus(k[i])
+            else:
+                k = self.window_list.keys()
+                i = (k.index(handle) - 1)
+                if i < 0:
+                    i = len(k) - 1
+                self.set_focus(k[i])
+        else:
+            #I don't know if this else has utility
+            pass
 
     def run(self):
         self.redraw()
--- src/pwin.py
+++ src/pwin.py
@@ -1,30 +1,40 @@
 import graphics
 from papp import papp
-    
+
 class PWin(object):
     def __init__(self,position):
         self.abs_position = position
         self.size = (position[2]-position[0],position[3]-position[1])
         self.canvas = graphics.Image.new(self.size)
-        papp.add_window(self)
+        self.handle = papp.add_window(self)
 
     def bind(self,key,funct):
         papp.bind(self,key,funct)
 
     def update_canvas(self):
         pass
-    
+
     def get_position(self):
         return self.abs_position
 
     def get_size(self):
         return self.size
-    
+
     def get_canvas(self):
         self.update_canvas()
         return self.canvas
 
     def redraw(self):
         papp.redraw_window(self)
-  
+        
+    def draw_focus(self):
+        pass
     
+    def clear_focus(self):
+        pass
+
+    def set_focus(self):
+        papp.set_focus(self.handle)
+
+    def release_focus(self, direction=0):
+        papp.release_focus(self.handle, direction)
