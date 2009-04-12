9e5a8638f40a8c6de7a10047e8dd724c2affe7f4
marcelobarrosalmeida <marcelobarrosalmeida@6f7cd686-250c-11de-9078-ef7f9e05793c>
2009-04-12 18:45:35 +0000

    listbox added

--- src/demo_pymgui.py
+++ src/demo_pymgui.py
@@ -1,6 +1,7 @@
 from papp import papp
 from pwin import PWin
 from random import randint
+from plistbox import PListBox
 
 class MyPWin(PWin):
     def __init__(self,position):
@@ -8,11 +9,13 @@ class MyPWin(PWin):
 
     def update_canvas(self): 
         self.canvas.clear((randint(0,255),randint(0,255),randint(0,255)))
-        return self.canvas
 
 w1 = MyPWin((0,0,50,50))
 w2 = MyPWin((50,50,100,100))
-w3 = MyPWin((100,100,150,150))
-w4 = MyPWin((150,150,200,200))
+
+items = ["A","B","C","D","E","F","G","H"]
+w3 = PListBox(position=(150,0,250,100),items=items)
+
+w4 = PListBox(position=(150,110,250,190),items=items)
 
 papp.run()
