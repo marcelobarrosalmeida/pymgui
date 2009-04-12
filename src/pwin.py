bf155b8749503b7bd86e23c1e04bce1c50e0ebdb
marcelobarrosalmeida <marcelobarrosalmeida@6f7cd686-250c-11de-9078-ef7f9e05793c>
2009-04-12 18:44:44 +0000

    creating functions for accessing internal values

--- src/pwin.py
+++ src/pwin.py
@@ -3,16 +3,25 @@ from papp import papp
     
 class PWin(object):
     def __init__(self,position):
-        self.position = position
+        self.abs_position = position
         self.size = (position[2]-position[0],position[3]-position[1])
         self.canvas = graphics.Image.new(self.size)
         papp.add_window(self)
 
     def bind(self,key,funct):
         papp.bind(self,key,funct)
-        
+
     def update_canvas(self):
-        # update canvas and return it, override this function
+        pass
+    
+    def get_position(self):
+        return self.abs_position
+
+    def get_size(self):
+        return self.size
+    
+    def get_canvas(self):
+        self.update_canvas()
         return self.canvas
   
     
